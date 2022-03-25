ae602b756d9ae192b145be6ba26881ce8348606a
昂! 无白 Herk <58321084+AlpHerk@users.noreply.github.com>
2022-03-25 19:34:02 +0800

    readme更新

--- NjtechLogin-IOS/ios_urllib3.py
+++
@@ -1,4 +0,0 @@
-import requests
-req = requests.request('GET', 'http://httpbin.org/get')
-if req.status_code: print("成功！！")
-print(requests.get("https://gitee.com/mersakk/stash/raw/master/getstash.py").text)
