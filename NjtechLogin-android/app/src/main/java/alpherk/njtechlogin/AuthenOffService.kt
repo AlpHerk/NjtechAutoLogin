5c759e0fb6e4aa27831ea8e2870e13328901dc2b
昂! 无白 Herk <58321084+AlpHerk@users.noreply.github.com>
2023-02-16 20:59:46 +0800

    白花三月 | 修复网页链接错误

---
+++ NjtechLogin-android/app/src/main/java/alpherk/njtechlogin/AuthenOffService.kt
@@ -0,0 +1,12 @@
+package alpherk.njtechlogin
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class AuthenOffService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
