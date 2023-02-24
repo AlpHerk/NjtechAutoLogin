a7d3d5904e3912bd8c6e767a218a78dadb118e79
昂! 无白 Herk <58321084+AlpHerk@users.noreply.github.com>
2023-02-24 23:13:24 +0800

    修复横竖屏切换闪退问题

---
+++ NjtechLogin-android/app/src/main/java/alpherk/njtechlogin/ui/BaseActivity.kt
@@ -0,0 +1,11 @@
+package alpherk.njtechlogin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+    }
+}
